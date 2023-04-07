4182246b9f8a53ab2167dcd2a59871fbf5fc3bd0
debluka <61825224+debluka@users.noreply.github.com>
2023-04-07 17:21:31 +0200

    Basic firestore setup

---
+++ databaseInit.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('firestoreKey.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://piratestreasure-a3944-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('/')
